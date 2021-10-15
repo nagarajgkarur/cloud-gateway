0e03d534fb2291b130c5a9ff1dc162f8fe336809
nagarajgkarur <nagaraj.g.karur@gmail.com>
2021-10-15 12:46:40 +0530

    cloud gateway first commit

---
+++ src/test/java/com/codeforcode/cloudgateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeforcode.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
